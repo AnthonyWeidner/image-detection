17f17bab64a020144246e17ebea001e146adf949
AnthonyWeidner <alweidner@ucdavis.edu>
2023-09-13 02:28:24 +0100

    Test changes

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -39,26 +39,14 @@ function App() {
         <h1>Image Classifier</h1>
         <ImageUpload />
       </header>
-    </div>
 
-
-
-    <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>ECS 170 Project</h1>
+       </header>
+
     </div>
+
+
   );
 }
 
